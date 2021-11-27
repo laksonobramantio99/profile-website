de9087067a7965911d8e4af430442b74a2829c30
Laksono Bramantio <laxbramantio@gmail.com>
2021-11-27 11:27:55 +0700

    add visit counter

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
+visit_count = 0
 
 @app.route("/")
 def home():
+    global visit_count
+    visit_count += 1
     return render_template('home.html')
+
+@app.route("/visit")
+def visit():
+    return "visit count: " + str(visit_count)
 
 # Comment before deploy
 # if __name__ == "__main__":
