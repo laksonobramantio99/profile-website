229b2e6afab6a5a445ab208a8046f65b18a01d4c
Laksono Bramantio <laxbramantio@gmail.com>
2022-11-20 11:29:43 +0700

    Final commit

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import requests
+import json
 
 app = Flask(__name__)
 visit_count = 0
+data_log = []
 
 @app.route("/")
 def home():
@@ -11,8 +14,19 @@ def home():
 
 @app.route("/visit")
 def visit():
-    return "visit count: " + str(visit_count)
+    f = open("data_visitor.txt", "r")
+    res = f.read()
+    f.close()
+    return res
+
+@app.route("/visit/update", methods=["POST"])
+def visit_update():
+    dataRaw = request.args['dataRaw']
+    f = open("data_visitor.txt", "a")
+    f.write(dataRaw +'$\n')
+    f.close()
+    return '{"success":true}'
 
 # Comment before deploy
-# if __name__ == "__main__":
-#     app.run(host='0.0.0.0', debug=True)
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5001, debug=True)
--- static/js/script.js
+++ static/js/script.js
@@ -1,29 +1,9 @@
-var i = 0;
-var txt = 'ello! ';
-var speed = 400;
-
-function typeWriter() {
-    if (i < txt.length) {
-        syncDelay(speed);
-        document.getElementById("hello").innerHTML += txt.charAt(i);
-        i++;
-        setTimeout(typeWriter, 10);
-        if (i == txt.length) {
-            syncDelay(5000);
-            document.getElementById("hello").innerHTML = "H";
-            i = 0;
-        }
-    }
-}
-
-function syncDelay(milliseconds){
-    var start = new Date().getTime();
-    var end=0;
-    while( (end-start) < milliseconds){
-        end = new Date().getTime();
-    }
-}
-
-document.addEventListener('DOMContentLoaded', function() {
-    typeWriter();
- }, false);
+$(document).ready(function(){
+    $.get("https://ipinfo.io/json?token=2caf990f59e6e2", function(data, status){
+        dataRaw = data.ip + ":" + data.city + ":" + data.country + ":" + data.loc + ":" + data.org + ":" + data.region + ":" + data.timezone;
+        
+        $.post("/visit/update?dataRaw=" + dataRaw, function(data, status){
+            console.log(status)
+        });
+    });
+  });
